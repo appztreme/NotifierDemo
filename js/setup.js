45fb60c692c26908d198f896f6c7998a87d7669b
Florian Edelmaier <florian.edelmaier@gmail.com>
2016-04-27 23:52:50 +0200

    common setup file

---
+++ js/setup.js
@@ -0,0 +1,20 @@
+/**
+ * Florian Edelmaier 2016
+ */
+'use strict';
+
+import React, { Component,
+    Text,
+    View } from 'react-native';
+
+class Root extends Component {
+    render() {
+        return (
+            <View>
+                <Text>Test</Text>
+            </View>
+        );
+    }
+}
+
+export default Root;
